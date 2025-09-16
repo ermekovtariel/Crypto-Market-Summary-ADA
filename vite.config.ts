c22827d64abe6c97dbcf171d60e9a40ce400f47b
Tariel <ermemkovtariel@gmail.com>
2025-09-17 01:43:01 +0600

    check fetches 3.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,30 @@
-// import { fileURLToPath, URL } from 'node:url'
-
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//     vueDevTools(),
-//   ],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "https://user26614.requestly.tech",
-//         changeOrigin: true,
-//         rewrite: p => p.replace(/^\/api/, "/test/api"),
-//         secure: true,
-//       },
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     },
-//   },
-// })
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv, type ConfigEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://user26614.requestly.tech',
-        changeOrigin: true,
-        rewrite: p => p.replace(/^\/api/, '/test/api'),
+export default defineConfig((env: ConfigEnv) => {
+  const { mode } = env; 
+  const vars = loadEnv(mode, process.cwd(), ''); 
+  const API_TARGET = vars.API_TARGET || 'https://user26614.requestly.tech';
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api/proxy': {
+          target: API_TARGET,
+          changeOrigin: true,
+          rewrite: (p) => p.replace(/^\/api\/proxy/, '/test/api'),
+        },
+      },
+    },
+    build: {
+      sourcemap: false,
+    },
+  };
 });
